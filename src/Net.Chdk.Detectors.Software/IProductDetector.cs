e18f827cdb06bc8f857348b0239e4a5483add9a5
Dmitry Shechtman <damnian@gmail.com>
2017-04-20 17:52:57 +0300

    Move ISoftwareDetector and IProductDetector to Net.Chdk.Detectors.Software.Core

--- src/Net.Chdk.Detectors.Software/IProductDetector.cs
+++
@@ -1,9 +0,0 @@
-﻿using Net.Chdk.Model.Software;
-
-namespace Net.Chdk.Detectors.Software
-{
-    public interface IProductDetector
-    {
-        ProductInfo GetProduct(string driveLetter);
-    }
-}
--- src/Net.Chdk.Detectors.Software/ISoftwareDetector.cs
+++
@@ -1,9 +0,0 @@
-﻿using Net.Chdk.Model.Software;
-
-namespace Net.Chdk.Detectors.Software
-{
-    public interface ISoftwareDetector
-    {
-        SoftwareInfo GetSoftware(string driveLetter);
-    }
-}
