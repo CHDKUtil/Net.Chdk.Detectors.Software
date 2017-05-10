9496484ce9dc0071138d2a0b79ff70b63da497f1
Dmitry Shechtman <damnian@gmail.com>
2017-05-10 20:57:43 +0300

    Implement GetHash()

--- src/Net.Chdk.Detectors.Software/BinarySoftwareDetector.cs
+++ src/Net.Chdk.Detectors.Software/BinarySoftwareDetector.cs
@@ -3,6 +3,7 @@ using Net.Chdk.Encoders.DancingBits;
 using Net.Chdk.Model.Card;
 using Net.Chdk.Model.Software;
 using Net.Chdk.Providers.Boot;
+using Net.Chdk.Providers.Crypto;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -11,21 +12,26 @@ namespace Net.Chdk.Detectors.Software
 {
     sealed class BinarySoftwareDetector : IInnerSoftwareDetector
     {
+        private const string HashName = "sha256";
+
         private ILogger Logger { get; }
         private IEnumerable<IInnerBinarySoftwareDetector> SoftwareDetectors { get; }
         private IBootProvider BootProvider { get; }
+        private IHashProvider HashProvider { get; }
 
-        public BinarySoftwareDetector(IEnumerable<IInnerBinarySoftwareDetector> softwareDetectors, IBootProvider bootProvider, ILoggerFactory loggerFactory)
+        public BinarySoftwareDetector(IEnumerable<IInnerBinarySoftwareDetector> softwareDetectors, IBootProvider bootProvider, IHashProvider hashProvider, ILoggerFactory loggerFactory)
         {
             Logger = loggerFactory.CreateLogger<BinarySoftwareDetector>();
             SoftwareDetectors = softwareDetectors;
             BootProvider = bootProvider;
+            HashProvider = hashProvider;
         }
 
         public SoftwareInfo GetSoftware(CardInfo cardInfo)
         {
             var rootPath = cardInfo.GetRootPath();
-            var diskbootPath = Path.Combine(rootPath, BootProvider.FileName);
+            var fileName = BootProvider.FileName;
+            var diskbootPath = Path.Combine(rootPath, fileName);
 
             Logger.LogTrace("Detecting software from {0}", diskbootPath);
 
@@ -35,19 +41,23 @@ namespace Net.Chdk.Detectors.Software
                 return null;
             }
 
-            var inBuffer = File.ReadAllBytes(diskbootPath);
-            var outBuffer = new byte[inBuffer.Length];
-            using (var inStream = new MemoryStream(inBuffer))
+            var encBuffer = File.ReadAllBytes(diskbootPath);
+            var decBuffer = new byte[encBuffer.Length];
+            using (var encStream = new MemoryStream(encBuffer))
             {
+                var hash = GetHash(encStream, fileName, HashName);
                 for (var version = 0; version <= DancingBitsEncoder.MaxVersion; version++)
                 {
-                    inStream.Seek(0, SeekOrigin.Begin);
-                    using (var outStream = new MemoryStream(outBuffer))
+                    encStream.Seek(0, SeekOrigin.Begin);
+                    using (var decStream = new MemoryStream(decBuffer))
                     {
-                        DancingBitsEncoder.Decode(inStream, outStream, version);
-                        var software = GetSoftware(outBuffer);
+                        DancingBitsEncoder.Decode(encStream, decStream, version);
+                        var software = GetSoftware(decBuffer);
                         if (software != null)
+                        {
+                            software.Hash = hash;
                             return software;
+                        }
                     }
                 }
             }
@@ -67,6 +77,25 @@ namespace Net.Chdk.Detectors.Software
                 .SelectMany(b => SeekAfterMany(buffer, b))
                 .Select(i => softwareDetector.GetSoftware(buffer, i))
                 .FirstOrDefault(s => s != null);
+        }
+
+        private SoftwareHashInfo GetHash(Stream stream, string fileName, string hashName)
+        {
+            return new SoftwareHashInfo
+            {
+                Name = HashName,
+                Values = GetHashValues(stream, fileName, hashName)
+            };
+        }
+
+        private Dictionary<string, string> GetHashValues(Stream stream, string fileName, string hashName)
+        {
+            var key = fileName.ToLowerInvariant();
+            var value = HashProvider.GetHashString(stream, HashName);
+            return new Dictionary<string, string>
+            {
+                { key, value }
+            };
         }
 
         private static IEnumerable<int> SeekAfterMany(byte[] buffer, byte[] bytes)
